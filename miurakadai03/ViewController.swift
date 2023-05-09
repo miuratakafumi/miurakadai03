13ca7f09569ac61c3915913172e773aea96d0e63
akio0911 <akio0911@gmail.com>
2023-05-09 19:02:17 +0900

    feedback

--- miurakadai03/ViewController.swift
+++ miurakadai03/ViewController.swift
@@ -8,38 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-//　テキストフィールドの配置
+    // テキストフィールドの配置
     @IBOutlet weak var textField1: UITextField!
     @IBOutlet weak var textField2: UITextField!
     
-//　ラベルの配置
+    // ラベルの配置
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var label2: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
 
-//  スイッチの配置
+    // スイッチの配置
     @IBOutlet weak var switch1: UISwitch!
     @IBOutlet weak var switch2: UISwitch!
     
-//  ボタンタップ時のアクション
+    // ボタンタップ時のアクション
     @IBAction func button(_ sender: Any) {
-        var number1 = Int(textField1.text ?? "") ?? 0
-        var number2 = Int(textField2.text ?? "") ?? 0
-        
-        if switch1.isOn {
-            number1 = number1 * (-1)
-        }
-        
-        if switch2.isOn {
-            number2 = number2 * (-1)
-        }
-        
-        label1.text = "\(number1)"
-        label2.text = "\(number2)"
-        
-        let sum = number1 + number2
-        totalLabel.text = "\(sum)"
-    }
-    
-}
+        let number1 = Int(textField1.text ?? "") ?? 0
+        let number2 = Int(textField2.text ?? "") ?? 0
 
+        let signedNumber1: Int
+        if switch1.isOn {
+            signedNumber1 = -number1
+        } else {
+            signedNumber1 = number1
+        }
+
+        let signedNumber2: Int
+        if switch2.isOn {
+            signedNumber2 = -number2
+        } else {
+            signedNumber2 = number2
+        }
+        
+        label1.text = "\(signedNumber1)"
+        label2.text = "\(signedNumber2)"
+        
+        totalLabel.text = "\(signedNumber1 + signedNumber2)"
+    }
+}
